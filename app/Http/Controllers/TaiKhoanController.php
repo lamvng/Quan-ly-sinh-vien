7921bc438afa280fbbd1e7fbbeec472d1c347dd5
khang15 <39270950+khang15@users.noreply.github.com>
2019-05-04 17:20:22 +0700

    Add files via upload

--- app/Http/Controllers/TaiKhoanController.php
+++ app/Http/Controllers/TaiKhoanController.php
@@ -55,37 +55,124 @@ class TaiKhoanController extends Controller
     public function getTimKiemTK() {
         return view('admin.qltk.timkiemtk');
     }
-    public function postTimKiemTK() {
+    public function postTimKiemTK(Request $request) {
+        $tukhoa = $request->tukhoa;
+        $tieuchi = $request->tieuchi;
+        $taikhoan = TaiKhoan::where($tieuchi,'=',$tukhoa)->get();
+
+        // if($tieuchi == 'hoTen') {
+        //     $taikhoan = TaiKhoan::where('hoTen',$tukhoa);
+        // }
+        return view('admin.qltk.timkiemtk',['taikhoan'=>$taikhoan]);
 
     }
 
-    public function getXemTK() {
-        return view('admin.qltk.xemtk');
+    public function getXemTK($username) {
+        $user = TaiKhoan::find($username);
+        return view('admin.qltk.xemtk',['user'=>$user]);
     }
 
-    public function getSuaTK() {
-        return view('admin.qltk.suatk');
+    public function getSuaTK($username) {
+        $user = TaiKhoan::find($username);
+        return view('admin.qltk.suatk',['user'=>$user]);
     }
-    public function postSuaTK() {
+    public function postSuaTK(Request $request, $username) {
+        $taikhoan = TaiKhoan::find($username); 
+        $this->validate($request, 
+            [
+                'hoTen' => 'required',
+                'chucVu' => 'required',
+                'gioiTinh' => 'required',
+                'email' => 'required|email',
+                'sdt' => 'required'
+            ], 
+            [
+                'hoTen.required' => 'Bạn chưa nhập họ tên',
+                'chucVu.required' => 'Bạn chưa nhập chức vụ' ,
+                'gioiTinh.required' => 'Bạn chưa nhập giới tính' ,
+                'email.required' => 'Bạn chưa nhập email' ,
+                'email.email' => 'Bạn nhập sai định dạng email',
+                'sdt.required' => 'Bạn chưa nhập số điện thoại',
+            ]);
+        $taikhoan->hoTen = $request->hoTen;
+        $taikhoan->gioiTinh = $request->gioiTinh;
+        $taikhoan->email = $request->email;
+        $taikhoan->chucVu = $request->chucVu;
+        $taikhoan->sdt = $request->sdt;
 
+        if($request->password != null) {
+            $this->validate($request,
+            [
+                'password' => 'min:8',
+                'passwordagain' => 'required|same:password',
+            ],
+            [
+                'password.min' => 'Mật khẩu ngắn hơn mức cho phép',
+                'passwordagain.required' => 'Bạn chưa nhập lại mật khẩu',
+                'passwordagain.same' => 'Mật khẩu không trùng',
+            ]);
+            $taikhoan->password = bcrypt($request->password);
+        }
+
+        $taikhoan->save();  
+
+        return redirect('admin/qltk/suatk/'.$username)->with('thongbao','Sửa thành công');  
     }
 
-    public function getXoaTK() {
-        return view('admin.qltk.xoatk');
-    }
-    public function postXoaTK() {
-
-    }
-
-    public function getXemTKCN() {
-        return view('admin.qltkcn.xemtkcn');
+    public function getXoaTK($username) {
+        $taikhoan = TaiKhoan::find($username);
+        $taikhoan->delete();
+        return view('admin.home')->with('thongbao','Xoá thành công');
     }
 
-    public function getSuaTKCN() {
-        return view('admin.qltkcn.suatkcn');
+    public function getXemTKCN($username) {
+        $user = Auth::user();
+        return view('admin.qltkcn.xemtkcn',['user'=>$user]);
     }
-    public function postSuaTKCN() {
 
+    public function getSuaTKCN($username) {
+        $user = Auth::user();
+        return view('admin.qltkcn.suatkcn',['user'=>$user]);
+    }
+    public function postSuaTKCN(Request $request, $username) {
+        $taikhoan = Auth::user(); 
+        $this->validate($request, 
+            [
+                'hoTen' => 'required',
+                'gioiTinh' => 'required',
+                'email' => 'required|email',
+                'sdt' => 'required'
+            ], 
+            [
+                'hoTen.required' => 'Bạn chưa nhập họ tên',
+                'gioiTinh.required' => 'Bạn chưa nhập giới tính' ,
+                'email.required' => 'Bạn chưa nhập email' ,
+                'email.email' => 'Bạn nhập sai định dạng email',
+                'sdt.required' => 'Bạn chưa nhập số điện thoại',
+            ]);
+
+        $taikhoan->gioiTinh = $request->gioiTinh;
+        $taikhoan->email = $request->email;
+        $taikhoan->chucVu = $request->chucVu;
+        $taikhoan->sdt = $request->sdt;
+
+        if($request->password != null) {
+            $this->validate($request,
+            [
+                'password' => 'min:8',
+                'passwordagain' => 'required|same:password',
+            ],
+            [
+                'password.min' => 'Mật khẩu ngắn hơn mức cho phép',
+                'passwordagain.required' => 'Bạn chưa nhập lại mật khẩu',
+                'passwordagain.same' => 'Mật khẩu không trùng',
+            ]);
+            $taikhoan->password = bcrypt($request->password);
+        }
+
+        $taikhoan->save();  
+
+        return redirect('admin/qltk/suatk/'.$username)->with('thongbao','Sửa thành công');  
     }
 
 
@@ -128,4 +215,5 @@ class TaiKhoanController extends Controller
         Auth::logout();
         return redirect('dangnhap');
     }
+
 }
